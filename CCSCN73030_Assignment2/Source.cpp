aa32b8a39bea9a6fbed999221dc46c20c990a770
Arun Kumar Thulaseedharan Pillai <Athulaseedharan0760@conestogac.on.ca>
2022-09-23 04:51:58 -0400

    Initial Commit- Created Function to Read Student Data and to push it into vector thorugh Student_data Sturct

--- CCSCN73030_Assignment2/Source.cpp
+++ CCSCN73030_Assignment2/Source.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include<sstream>
 
+
+
+using namespace std;
+
+//#define PRE_RELEASE
+
+typedef struct STUDENT_DATA {
+	string FName;
+	string LName;
+#ifdef PRE_RELEASE
+	string Email;
+#endif // RELEASE
+
+
+}Student;
+
+
+
+std::vector<Student> ReadData(string Filename) {
+
+	ifstream DataFile(Filename);
+	string line;
+	Student S1;
+
+	vector<Student> Vec_Data;
+
+	if (DataFile.is_open())
+	{
+
+		while (getline(DataFile, line)) {
+
+			istringstream Data(line);
+
+			getline(Data, line, ',');
+			S1.LName = line;
+
+			getline(Data, line, ',');
+			S1.FName = line;
+
+#ifdef PRE_RELEASE
+			getline(Data, line, ',');
+			S1.Email = line;
+#endif // RELEASE
+
+			Vec_Data.push_back(S1);
+		}
+
+		/*for (int i = 0; i < Vec_Data.size(); i++) {
+			cout << "Name : " << Vec_Data[i].FName << " " << Vec_Data[i].LName << endl;
+		}*/
+	}
+	return Vec_Data;
+}
 
 int main() {
+
+
+
+
+
 
 
 }
